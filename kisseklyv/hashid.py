ea110b4cf80f1358f4032de3aeaab75ac289ecb9
Hannes Jensen <hannes@hannesjensen.com>
2020-02-19 16:13:40 +0100

    add logging for hashing

--- kisseklyv/hashid.py
+++ kisseklyv/hashid.py
@@ -6,12 +6,24 @@ HASH_ID_PADDING = 1000  # lägg till id för att få längre strängar
 
 
 def get_hashid_from_id(id: int) -> str:
-    id += HASH_ID_PADDING
-    cfg = config.Config()
-    hashids = Hashids(salt=cfg.HASHIDS_SALT)
-    return hashids.encode(id)
+    try:
+        id += HASH_ID_PADDING
+        cfg = config.Config()
+        hashids = Hashids(salt=cfg.HASHIDS_SALT)
+        return hashids.encode(id)
+    except Exception:
+        print("Error in get_hashid_from_id")
+        print(f"id: {id}")
+        print(f"Salt: {cfg.HASHIDS_SALT}")
+        return None
 
 def get_id_from_hashid(hashid: str) -> int:
-    cfg = config.Config()
-    hashids = Hashids(salt=cfg.HASHIDS_SALT)
-    return hashids.decode(hashid)[0] - HASH_ID_PADDING
+    try:
+        cfg = config.Config()
+        hashids = Hashids(salt=cfg.HASHIDS_SALT)
+        return hashids.decode(hashid)[0] - HASH_ID_PADDING
+    except Exception:
+        print("Error in get_id_from_hashid")
+        print(f"hashid: {hashid}")
+        print(f"Salt: {cfg.HASHIDS_SALT}")
+        return None
